aca5e479f5f2167699e68f6ef6f9469447baae80
Budaxcomel <96898071+Budaxcomel@users.noreply.github.com>
2024-08-27 21:51:27 +0800

    Update tokens.py

--- configs/tokens.py
+++ configs/tokens.py
@@ -1,4 +1,4 @@
 api_hash= "366347107f54aabc951cfa9d3c2fb2ce" # api hash 
 api_id= "10115546" #api id
-bot_token = "7272555814:AAHSkbRbrHOlOnaXZtHfmYn0IEWMh21VyVU"
+bot_token = "7373099845:AAFg9Kv1_oAO8gTux3Xcqipc470Fpxooq04"
 bin_checkurl = "https://lookup.binlist.net/"
